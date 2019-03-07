8d7c0a0a5bb2b4524855f43f5e77535f543abf71
Seba <yofreeman@wp.pl>
2019-03-07 11:41:35 +0100

    set components

--- app/src/main/java/buying/tickets/speech/view/SpeechMainActivity.java
+++ app/src/main/java/buying/tickets/speech/view/SpeechMainActivity.java
@@ -9,6 +9,7 @@ package buying.tickets.speech.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import buying.tickets.R;
 
@@ -19,12 +20,33 @@ public class SpeechMainActivity extends AppCompatActivity {
 
     private static SpeechMainActivity speechMainActivity;
 
+    private Button buyTicketButton;
+    private Button ticketControlButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech_main);
 
         speechMainActivity = this;
+
+        setComponents();
+    }
+
+    private void setComponents() {
+        buyTicketButton = findViewById(R.id.speech_main_buy_ticketButton);
+        ticketControlButton = findViewById(R.id.speech_main_ticket_controlButton);
+
+        setBuyTicketsButton();
+        setTicketControlButtonButton();
+    }
+
+    private void setBuyTicketsButton() {
+        buyTicketButton.setClickable(false);
+    }
+
+    private void setTicketControlButtonButton() {
+        ticketControlButton.setClickable(false);
     }
 
     public static SpeechMainActivity getInstance() {
