72ede22f0ec885aaa61d3c23ce011bf17ae24a42
Seba <yofreeman@wp.pl>
2019-03-09 16:18:22 +0100

    updated  speech summary module

--- app/src/main/java/buying/tickets/speech/contract/SpeechSummaryInterface.java
+++ app/src/main/java/buying/tickets/speech/contract/SpeechSummaryInterface.java
@@ -1,7 +1,7 @@
 /*
  * Created by Sebastian Paciorek on 9.3.2019
  * Copyright (c) 2019.  All rights reserved.
- * Last modified 09.03.19 14:49
+ * Last modified 09.03.19 16:18
  */
 
 package buying.tickets.speech.contract;
